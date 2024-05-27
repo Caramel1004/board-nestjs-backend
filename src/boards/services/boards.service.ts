7667df62974c7692065c4e25d002308603505ef5
Caramel1004 <Caramel1004.js@gmail.com>
2024-05-28 04:09:20 +0900

    ♻️ [refactor] CRUD 쿼리 메소드로 변경
    [refactor] CRUD 쿼리 메소드로 변경

--- src/boards/services/boards.service.ts
+++ src/boards/services/boards.service.ts
@@ -1,41 +1,56 @@
-import { Injectable } from '@nestjs/common';
-import { Board, BoardStatus } from "../boards.model";
-import { randomUUID } from 'crypto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { BoardStatus } from "../boards.model";
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { BoardEntity } from '../models/entity/board.entity';
 
 @Injectable()
 export class BoardsService {
-    private boards: Board[] = [];
-    constructor(
-        @InjectRepository(BoardEntity) 
-        private boardsRepository: Repository<BoardEntity>
-    ) {}
 
+    constructor(
+        @InjectRepository(BoardEntity)
+        private boardsRepository: Repository<BoardEntity>
+    ) { }
+
+    /* 모든 게시물 조회 */
     async getAllBoards(): Promise<BoardEntity[]> {
         return await this.boardsRepository.find();
     }
 
-    getPostById(id: String): Board {
-        return this.boards.find(post => post.id === id);
-    }
+    /* 특정 게시물 조회 */
+    async getPostById(id: String): Promise<BoardEntity> {
+        console.log(id);
+        const post = await this.boardsRepository
+            .createQueryBuilder('board')
+            .where('board.id = :id', { id: id })
+            .getOne();
 
-    createPost({ title, description }): Board {
-        const board: Board = {
-            id: randomUUID(),
-            title,
-            description,
-            status: BoardStatus.PUBLIC
+        if (!post) {
+            throw new NotFoundException(`게시물이 없습니다.`);
         }
 
-        this.boards.push(board);
-
-        return board;
+        return post;
     }
 
-    deletePostById(id: String) {
-        this.boards = [...this.boards.filter(post => post.id !== id)];
+    /* 게시물 생성 */
+    async createPost({ title, description }): Promise<BoardEntity> {
+        const board: BoardEntity = this.boardsRepository.create({
+            title: title,
+            description: description,
+            status: BoardStatus.PUBLIC
+        });
+
+        return await this.boardsRepository.save(board);
+    }
+
+    /* 특정 게시물 삭제*/
+    async deletePostById(id: String) {
+
+        await this.boardsRepository.createQueryBuilder()
+            .delete()
+            .from(BoardEntity)
+            .where("id = :id", { id: id })
+            .execute();
 
         return {
             code: 200,
@@ -43,22 +58,18 @@ export class BoardsService {
         }
     }
 
-    updatePostById(id, { title, description }) {
-        let post: Board = {
-            id,
-            title,
-            description,
-            status: this.boards.filter(post => post.id === id)[0].status
-        }
+    /* 특정 게시물 업데이트 */
+    async updatePostById(id: String, { title, description }) {
 
-        this.boards = this.boards.map(board => {
-            if (board.id === id) {
-                board = {
-                    ...post
-                }
-            }
-            return board;
-        });
+        await this.boardsRepository.createQueryBuilder()
+            .update(BoardEntity)
+            .set({
+                title: title,
+                description: description,
+                status: BoardStatus.PUBLIC
+            })
+            .where('id=:id', { id: id })
+            .execute()
 
         return {
             code: 200,
