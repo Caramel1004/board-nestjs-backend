15f3f8da5218205c8620c3f254bb8a2e6b37086f
Caramel1004 <Caramel1004.js@gmail.com>
2024-05-28 04:10:47 +0900

    🔧 [config] typeorm 모듈 추가
    ==

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { BoardsController } from './controllers/boards.controller';
 import { BoardsService } from './services/boards.service';
+import { BoardEntity } from './models/entity/board.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([BoardEntity]),
+        TypeOrmModule.forFeature([Repository<BoardEntity>])
+    ],
     controllers: [BoardsController],
     providers: [BoardsService]
 })
